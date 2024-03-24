4670a9cc1ea934bb81a34bc58f67a8432a4c3bac
unknown <ednaldocaetanoguedes@gmail.com>
2024-03-24 15:14:10 -0300

    Layout: implementação dos modulares de layout (header, footer); aprimoramento de layout [implementation of layout modules (header, footer); layout improvement]

--- assets/js/layout/index.js
+++ assets/js/layout/index.js
@@ -1,12 +1,18 @@
+import Footer from "./footer";
+import Header from "./header";
 import Main from "./main";
 
 
 export default function Layout({ children }) {
   return (
     <div className={"layout"}>
+      <Header />
+
       <Main>
         {children}
       </Main>
+
+      <Footer />
     </div>
   )
 }
